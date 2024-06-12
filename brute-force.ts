ec6d9af9980e1a192aa55057a70dd3e2b2465527
Lucky7Tb <luckytri101@gmail.com>
2024-06-12 09:44:53 +0700

    not record history of jump path

--- brute-force.ts
+++ brute-force.ts
@@ -1,9 +1,10 @@
-const generatedJumpPath: number[][] = [];
+const result: number[][] = [];
+let minimumJump = 0;
 
 function main() {
   const arr = [1, 2, 4, 5];
   generateJumpPath(arr, 0, []);
-  console.log(minimumPossibleJump(generatedJumpPath));
+  console.log(result);
 }
 
 function generateJumpPath(
@@ -19,34 +20,16 @@ function generateJumpPath(
     if (nextJump < arr.length) {
       const newJumpPath = [...jumpPath, nextJump];
       if (nextJump === arr.length - 1) {
-        generatedJumpPath.push(newJumpPath);
+        if (newJumpPath.length < minimumJump || minimumJump === 0) {
+          minimumJump = newJumpPath.length;
+          result.length = 0;
+          result.push(newJumpPath);
+        }
       } else {
         generateJumpPath(arr, nextJump, newJumpPath);
       }
     }
   }
-}
-
-function minimumPossibleJump(arr: number[][]) {
-  const result: number[][] = [];
-  let minimumJump = 0;
-
-  for (let index = 0; index < generatedJumpPath.length; index++) {
-    const jumpPath = generatedJumpPath[index];
-
-    if (jumpPath.length < minimumJump || minimumJump === 0) {
-      minimumJump = jumpPath.length;
-      result.length = 0;
-      result.push(jumpPath);
-      continue;
-    }
-
-    if (jumpPath.length === minimumJump) {
-      result.push(jumpPath);
-    }
-  }
-
-  return result;
 }
 
 main();
